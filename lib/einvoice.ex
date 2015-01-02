4c6d00351f284d554e045d1351ddd50863d4a145
Jordan Dimov <jdimov@a115.co.uk>
2015-01-02 15:36:54 +0000

    Functionality to calculate line totals, VAT and totals.

--- lib/einvoice.ex
+++ lib/einvoice.ex
@@ -1,2 +1,15 @@
 defmodule Einvoice do
+
+  def add_up([]), do: 0
+  def add_up([head|tail]), do: head + add_up(tail)
+
+  @spec calc_vat(number(), number()) :: number()
+  def calc_vat(total, vat \\ 20), do: vat * total / 100
+
+  @spec add_vat(number(), number()) :: number()
+  def add_vat(total, vat \\ 20), do: total + calc_vat(total, vat)
+
+  @spec line_total(number(), number()) :: number()
+  def line_total(unit_price, qty), do: unit_price * qty
+
 end
--- test/einvoice_test.exs
+++ test/einvoice_test.exs
@@ -1,7 +1,64 @@
 defmodule EinvoiceTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  import Einvoice
+
+  test "add_up correctly returns zero for empty list" do
+    assert add_up([]) == 0
   end
+
+  test "add_up correctly returns sum of integer items in non-empty list" do
+    assert add_up([2,3,4,5,6,7]) == 27
+  end
+
+  test "calc_vat returns zero for zero amount" do
+    assert calc_vat(0) == 0
+  end
+
+  test "calc_vat returns correct default (20%) vat for amount" do
+    assert calc_vat(80) == 16
+  end
+
+  test "calc_vat returns correct vat with decimals" do
+    assert calc_vat(81) == 16.2
+  end
+
+  test "calc_vat returns correct custom vat for amount" do
+    assert calc_vat(70, 21.5) == 15.05
+  end
+
+  test "calc_vat returns zero when vat is 0%" do
+    assert calc_vat(75, 0) == 0
+  end
+
+  test "add_vat returns correct amount with default (20%) vat" do
+    assert add_vat(100) == 120
+  end
+
+  test "add_vat returns zero when amount is 0" do
+    assert add_vat(0) == 0
+  end
+
+  test "add_vat returns correct amount with custom vat" do 
+    assert add_vat(70, 18) == 82.6
+  end
+
+  test "add_vat returns same amount when vat is 0%" do
+    assert add_vat(75, 0) == 75
+  end
+
+  test "line_total returns zero when quantity is 0" do
+    assert line_total(15.2, 0) == 0
+  end
+
+  test "line_total returns zero when unit price is 0" do
+    assert line_total(0, 3) == 0
+  end
+
+  test "line_total returns correct total for non-zero quantity and unit price" do
+    # Can't check exactly because actual result is 45.599999999999994 for some reason
+    assert line_total(15.2, 3) >= 45.59
+    assert line_total(15.2, 3) <= 45.60
+  end
+
 end
